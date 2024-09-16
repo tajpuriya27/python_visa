152f89e8953fcbf2c017cadf6b617b816506c888
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-09-16 20:42:18 +0545

    wip: working on something idk

--- encryption.py
+++ encryption.py
@@ -6,7 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PUBLIC_KEY = os.getenv('PUBLIC_KEY')
+# PUBLIC_KEY= os.getenv('PUBLIC_KEY')
+
+PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpigN3/5Ti/WJk51pbPQdpCe96
+TPVoeMAk/cUlAPpYh8zGpr6zssbM11Je1SoQTiuipxIL+c0oGXti8vLzln3yfS+N
+56wuSh0Hyt1Z+waSx6IDFlfzImEtq8m1osS32B83HRiFZbeKB8QIRJhZil1pJSzM
+sg0Y0QmDyv1yR4FzIQIDAQAB
+-----END PUBLIC KEY-----"""
+
 PASSWORD = os.getenv('PASSWORD')
 
 
